e9025ac6e496a4063d1e1ceb33ff381bf11a9023
sbx_user1059 <sbx_user1059@ip-10-12-166-195.eu-west-2.compute.internal>
2018-05-24 14:55:10 +0000

    Thu May 24 14:55:10 UTC 2018

--- lib/solutions/sum.py
+++ lib/solutions/sum.py
@@ -1,3 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def sum(x, y):
-    raise NotImplementedError()
+    if x > 100 or x < 0:
+        raise ValueError("x value out of range")
+    if y > 100 or y < 0:
+        raise ValueError("y value out of range")
+
+    return x + y
