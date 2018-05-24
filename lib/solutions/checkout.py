df2b8878049966449a7e811a5ef0f02773da8de3
sbx_user1059 <sbx_user1059@ip-10-12-166-195.eu-west-2.compute.internal>
2018-05-24 15:14:29 +0000

    Thu May 24 15:14:29 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,6 +1,55 @@
+# Our price table and offers: 
+# +------+-------+----------------+
+# | Item | Price | Special offers |
+# +------+-------+----------------+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |                |
+# +------+-------+----------------+
 
+
+# Notes: 
+#  - For any illegal input return -1
+
+# In order to complete the round you need to implement the following method:
+#      checkout(String) -> Integer
+
+# Where:
+#  - param[0] = a String containing the SKUs of all the products in the basket
+#  - @return = an Integer representing the total checkout value of the items 
+
+from collections import Counter
+
+prices = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+}
+
+offers = {
+    "A": (3, 20),
+    "B": (2, 45),
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    products = Counter(skus)
+    total = 0
+
+    for product, count in products.iteritems():
+        if product not in prices:
+            return -1
+
+        total += (prices[product] * count) 
+        offer = offers.get(product)
+        if offer:
+            discount = int(count / offers[product][0]) * offers[product][1]
+            total -= discount 
+    
+    return total
+
+
+
