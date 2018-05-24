b9737b3afa8cc3c7dbf3d5fa202d0047e1c58d9e
sbx_user1059 <sbx_user1059@ip-10-12-166-195.eu-west-2.compute.internal>
2018-05-24 15:50:44 +0000

    Thu May 24 15:50:44 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -8,9 +8,39 @@
 # | D    | 15    |                        |
 # | E    | 40    | 2E get one B free      |
 # | F    | 10    | 2F get one F free      |
+# | G    | 20    |                        |
+# | H    | 10    | 5H for 45, 10H for 80  |
+# | I    | 35    |                        |
+# | J    | 60    |                        |
+# | K    | 80    | 2K for 150             |
+# | L    | 90    |                        |
+# | M    | 15    |                        |
+# | N    | 40    | 3N get one M free      |
+# | O    | 10    |                        |
+# | P    | 50    | 5P for 200             |
+# | Q    | 30    | 3Q for 80              |
+# | R    | 50    | 3R get one Q free      |
+# | S    | 30    |                        |
+# | T    | 20    |                        |
+# | U    | 40    | 3U get one U free      |
+# | V    | 50    | 2V for 90, 3V for 130  |
+# | W    | 20    |                        |
+# | X    | 90    |                        |
+# | Y    | 10    |                        |
+# | Z    | 50    |                        |
 # +------+-------+------------------------+
 
 from collections import Counter
+
+offers = {
+    "A": [(5, 50), (3, 20)],
+    "B": [(2, 15)],
+    "H": [(10, 20), (5, 5)],
+    "K": [(2, 10)],
+    "P": [(5, 50)],
+    "Q": [(3, 10)],
+    "V": [(3, 20), (2, 10)],
+}
 
 prices = {
     "A": 50,
@@ -19,12 +49,48 @@ prices = {
     "D": 15,
     "E": 40,
     "F": 10,
+    "G": 20,
+    "H": 10,
+    "I": 35,
+    "J": 60,
+    "K": 80,
+    "L": 90,
+    "M": 15,
+    "N": 40,
+    "O": 10,
+    "P": 50,
+    "Q": 30,
+    "R": 50,
+    "S": 30,
+    "T": 20,
+    "U": 40,
+    "V": 50,
+    "W": 20,
+    "X": 90,
+    "Y": 10,
+    "Z": 50,
 }
 
-offers = {
-    "A": [(5, 50), (3, 20)],
-    "B": [(2, 15)],
+freebies = {
+    "E": (2, "B"),
+    "F": (2, "F"),
+    "N": (3, "M"), 
+    "R": (3, "Q"),
+    "U": (3, "U"),
 }
+
+def apply_freebie(products):
+    for sku, rule in freebies.iteritems():
+        counter = products.get(sku)
+        if counter:
+            freebie = int(products[sku] / rule[0])
+            freebie_count = products.get(rule[1])
+            if freebie_count:
+                products[rule[1]] -= freebie
+                if products[rule[1]] < 0:
+                    products[rule[1]] = 0
+    return products
+
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -32,15 +98,7 @@ def checkout(skus):
     products = Counter(skus)
     total = 0
 
-    if products.get("E") > 1:
-        freebie = int(products["E"] / 2)
-        b_count = products.get("B")
-        if b_count:
-            products["B"] -= freebie
-
-    if products.get("F") > 2:
-        freebie = int(products["F"] / 3)
-        products["F"] -= freebie
+    products = apply_freebie(products)
 
     for product, count in products.iteritems():
         if product not in prices:
